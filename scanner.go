f741c7af0c63247ae93fcb32796fba05dd08afff
Detlef Stern <ds@zettelstore.de>
2022-06-04 01:18:55 +0200

    Initial version of token scanner

--- scanner.go
+++ scanner.go
@@ -11,101 +11,206 @@
 package sxpf
 
 import (
-	"fmt"
+	"bytes"
 	"io"
+	"unicode"
 )
 
-// S-Expression scanner state machine.
-// It is loosely modeled after the JSON value parser state machine of the Go
-// standard library.
-//
-// To be more exact, "scanner" is both a lexical analyzer *and* some kind of
-// parser. An ordinary scanner is effectively equivalent to a final state machine,
-// which does not allow recursion. Since an s-expression is defined recursively,
-// the "scanner" defined here maintains a stack where it tracks nested lists and
-// similar s-expression elements.
+// TokenType enumerates the concrete type of token.
+type TokenType int
 
-// A SyntaxError is a description of a s-expression syntax error.
-type SyntaxError struct {
-	text string
-}
-
-func (e *SyntaxError) Error() string { return e.text }
-
-// stepResult is the return value of the transition function of a scanner ("step").
-type stepResult int
-
-// Constant values for stepResult.
+// Constants for TokenType.
 const (
-	scanContinue stepResult = iota // Uninteresting rune
-
-	// Values that signals an end state.
-	scanEnd
-	scanError // Some error detected, error value is in scanner.err
+	TokErr        TokenType = iota // Error
+	TokEOF                         // End of Input
+	TokLeftParen                   // (
+	TokPeriod                      // .
+	TokRightParen                  // )
+	TokLeftBrack                   // [
+	TokRightBrack                  // ]
+	TokSymbol                      // a symbol
+	TokString                      // "..."
 )
 
-// parseState is an indicator about the current state when the scanner enters a
-// recursive element, like a left parenthesis, which indicated a nested list.
-type parseState int
+// Token is the result of calling a scanner.
+type Token struct {
+	Typ TokenType
+	Val string
+}
 
-// Constand values for parseState.
+// Scanner are returning Token from a Reader.
+type Scanner struct {
+	rd  Reader
+	pos uint64 // current bye position in Reader
+	err error
+}
+
+// NewScanner creates a new scanner.
+func NewScanner(rd Reader) *Scanner {
+	return &Scanner{rd, 0, nil}
+}
+
+func (s *Scanner) Error() error { return s.err }
+
 const (
-	parseListValue parseState = iota
+	chErr rune = -1
+	chEOF rune = 0
 )
 
-// A scanner is the state machine to scan s-expressions.
-type scanner struct {
-	err        error
-	step       func(rune) stepResult
-	parseStack []parseState
-}
-
-func newScanner() *scanner {
-	s := &scanner{}
-	s.reset()
-	return s
-}
-
-// reset prepared the scanner to be used from a safe / initial state.
-func (s *scanner) reset() {
-	s.err = nil
-	s.step = s.stepError
-	s.parseStack = s.parseStack[0:0]
-}
-
-func (s *scanner) checkEOI() stepResult {
+func (s *Scanner) read() rune {
 	if s.err != nil {
-		return scanError
+		return chErr
 	}
-	return scanEnd
+	ch, width, err := s.rd.ReadRune()
+	if err != nil {
+		if err == io.EOF {
+			return chEOF
+		}
+		s.err = err
+		return chErr
+	}
+	s.pos += uint64(width)
+	return ch
 }
 
-func (s *scanner) checkValid(r Reader) error {
-	s.reset()
+func (s *Scanner) Next() Token {
+	ch := s.read()
+	for unicode.IsSpace(ch) {
+		ch = s.read()
+	}
+	switch ch {
+	case chEOF:
+		return Token{TokEOF, ""}
+	case chErr:
+		return Token{TokErr, s.err.Error()}
+	case '(':
+		return Token{TokLeftParen, "("}
+	case '.':
+		return Token{TokPeriod, "."}
+	case ')':
+		return Token{TokRightParen, ")"}
+	case '[':
+		return Token{TokLeftBrack, "["}
+	case ']':
+		return Token{TokRightBrack, "]"}
+	case '"':
+		return s.nextString()
+	}
+	if unicode.In(ch, unicode.C) {
+		// TODO: invalid unicode char at position
+		s.err = io.EOF
+		return Token{TokErr, s.err.Error()}
+	}
+	return s.nextSymbol(ch)
+}
+
+func (s *Scanner) nextSymbol(ch rune) Token {
+	var buf bytes.Buffer
 	for {
-		ch, _, err := r.ReadRune()
-		if err != nil {
-			if err == io.EOF {
-				break
+		buf.WriteRune(ch)
+		ch = s.read()
+		switch ch {
+		case chEOF, '(', '.', ')', '[', ']', '"':
+			err := s.rd.UnreadRune()
+			if err == nil {
+				return Token{TokSymbol, buf.String()}
 			}
-			return err
+			s.err = err
+			fallthrough
+		case chErr:
+			return Token{TokErr, s.err.Error()}
 		}
-		if s.step(ch) == scanError {
-			return s.err
+		if unicode.IsSpace(ch) {
+			// No need to unread, since space will be skipped next time
+			return Token{TokSymbol, buf.String()}
+		}
+		if unicode.In(ch, unicode.C) {
+			// TODO: invalid unicode char at position
+			s.err = io.EOF
+			return Token{TokErr, s.err.Error()}
 		}
 	}
-	if s.checkEOI() == scanError {
-		return s.err
-	}
-	return nil
 }
 
-// stepError is the error state transition function.
-func (*scanner) stepError(rune) stepResult { return scanError }
+func (s *Scanner) nextString() Token {
+	var buf bytes.Buffer
+	for {
+		ch := s.read()
+		switch ch {
+		case chEOF:
+			s.err = ErrMissingQuote
+			fallthrough
+		case chErr:
+			return Token{TokErr, s.err.Error()}
+		case '"':
+			return Token{TokString, buf.String()}
+		case '\\':
+			ch = s.read()
+			switch ch {
+			case chEOF:
+				s.err = ErrMissingQuote
+				fallthrough
+			case chErr:
+				return Token{TokErr, s.err.Error()}
+			case 't':
+				buf.WriteByte('\t')
+			case 'r':
+				buf.WriteByte('\r')
+			case 'n':
+				buf.WriteByte('\n')
+			case 'x':
+				s.parseRune(&buf, ch, 2)
+			case 'u':
+				s.parseRune(&buf, ch, 4)
+			case 'U':
+				s.parseRune(&buf, ch, 6)
+			default:
+				buf.WriteRune(ch)
+			}
+		default:
+			buf.WriteRune(ch)
+		}
+	}
+}
 
-func (s *scanner) error(ch rune, msg string) stepResult {
-	rs := string(ch)
-	s.step = s.stepError
-	s.err = &SyntaxError{fmt.Sprintf("invalid character %q %s", rs, msg)}
-	return scanError
+var xhexMap = map[rune]int{
+	'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+	'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15,
+	'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15,
+}
+
+func (s *Scanner) parseRune(buf *bytes.Buffer, curCh rune, numDigits int) {
+	var arr [8]rune
+	result := rune(0)
+loop:
+	for i := 0; i < numDigits; i++ {
+		ch := s.read()
+		switch ch {
+		case chEOF:
+			s.err = ErrMissingQuote
+			return
+		case chErr:
+			return
+		case '"':
+			xflushRunes(buf, &arr, i, curCh)
+			s.err = s.rd.UnreadRune()
+			break loop
+		}
+		arr[i] = ch
+		if hexVal, found := xhexMap[ch]; found {
+			result = (result << 4) + rune(hexVal)
+			continue
+		}
+		xflushRunes(buf, &arr, i, curCh)
+		return
+	}
+	buf.WriteRune(result)
+}
+
+func xflushRunes(buf *bytes.Buffer, arr *[8]rune, i int, ch rune) {
+	buf.WriteByte('\\')
+	buf.WriteRune(ch)
+	for j := 0; j < i; j++ {
+		buf.WriteRune(arr[j])
+	}
 }
--- scanner_test.go
+++ scanner_test.go
@@ -8,18 +8,64 @@
 // under this license.
 //-----------------------------------------------------------------------------
 
-package sxpf
+package sxpf_test
 
 import (
+	"bytes"
 	"strings"
 	"testing"
+
+	"github.com/t73fde/sxpf"
 )
 
 func TestValidScans(t *testing.T) {
 	t.Parallel()
-	s := newScanner()
-	err := s.checkValid(strings.NewReader(" "))
-	if err != nil {
-		t.Error(err)
+	s := sxpf.NewScanner(strings.NewReader(" "))
+	tok := s.Next()
+	if tok.Typ != sxpf.TokEOF {
+		t.Error(tok)
+	}
+}
+
+func TestScanner(t *testing.T) {
+	t.Parallel()
+	testcases := []struct {
+		src string
+		exp string
+	}{
+		{"", ""},
+		{"(", "("},
+		{"(.)[]", "(.)[]"},
+		{"a.", "a."},
+		{`""`, ``},
+		{`"a"`, `a`},
+		{`"\""`, `"`},
+		{`"\\"`, "\\"},
+		{`"\t"`, "\t"},
+		{`"\r"`, "\r"},
+		{`"\n"`, "\n"},
+		//{`"\x"`, `\x`}, {`"\x4"`, `\x4`}, {`"\x41"`, `A`}, {`"\x4g"`, `\x4g`},
+		//{`"\u"`, `\u`}, {`"\u0"`, `\u0`}, {`"\u00"`, `\u00`}, {`"\u004"`, `"u004"`}, {`"\u0042"`, `B`},
+		//{`"\U"`, `\U`}, {`"\U0"`, `\U0`}, {`"\U00"`, `\U00`}, {`"\U000"`, `"U000"`}, {`"\U0000"`, `"U0000"`},
+		//{`"\U00004"`, `U00004`}, {`"\U000043"`, `C`},
+	}
+	for i, tc := range testcases {
+		var buf bytes.Buffer
+		s := sxpf.NewScanner(strings.NewReader(tc.src))
+		for {
+			tok := s.Next()
+			if tok.Typ == sxpf.TokEOF {
+				break
+			}
+			if tok.Typ == sxpf.TokErr {
+				buf.WriteByte('E')
+				break
+			}
+			buf.WriteString(tok.Val)
+		}
+		got := buf.String()
+		if got != tc.exp {
+			t.Errorf("%d: %q -> %q, but got %q", i, tc.src, tc.exp, got)
+		}
 	}
 }
